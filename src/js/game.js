52171e43c025aaf56aff2797d12cf3bf6d9cfe5d
Domka28 <119631616+Domka28@users.noreply.github.com>
2023-10-23 16:13:11 +0200

    Changes in JS, add responsive in CSS (in progress)

--- src/js/game.js
+++ src/js/game.js
@@ -1,22 +1,10 @@
 const gameContainer = document.querySelector(".game-container")
 const scoreContainer = document.querySelector(".score-container")
-const level = document.querySelector(".level")
-const pairs = document.querySelector(".pairs")
-const uncoveredPairs = document.querySelector(".uncovered-pairs")
-const attempts = document.querySelector(".attempts")
-const allAttempts = document.querySelector(".all-attempts")
-const endBtn = document.querySelector(".end-btn")
-const newGameBtn = document.querySelector(".new-game-btn")
-
-
-
 const quantity = localStorage.getItem("quantity");
-console.log("ilość kart", quantity)
 const quantityOfCards = 72;
 let arrayOfRandomNumbers = [];
 let pairCounter = 0;
 let attemptsCounter = 0;
-// console.log(pairCounter)
 
 function getRandomNumbers() {
     while (arrayOfRandomNumbers.length < quantity) {
@@ -45,38 +33,40 @@ function renderImages() {
     });
 }
 
-
 function setGameContainerWidth() {
     const elementCount = newDuplicateArrayOfRandomNumbers.length;
-
-    if (elementCount === 16) {
-        gameContainer.style.width = "600px";
-        scoreContainer.style.width = "520px";
-        scoreContainer.style.fontSize = "1em";
-    } else if (elementCount === 24) {
-        gameContainer.style.width = "900px";
-        scoreContainer.style.width = "795px";
-        scoreContainer.style.fontSize = "1.2em";
-    } else if (elementCount === 32) {
-        gameContainer.style.width = "1100px";
-        scoreContainer.style.width = "1070px;";
-        scoreContainer.style.fontSize = "1.5em";
+    console.log(window.innerWidth)
+    if (window.innerWidth > 600) {
+        if (elementCount === 16) {
+            gameContainer.style.width = "600px";
+            scoreContainer.style.width = "520px";
+            scoreContainer.style.fontSize = "1em";
+        } else if (elementCount === 24) {
+            gameContainer.style.width = "900px";
+            scoreContainer.style.width = "795px";
+            scoreContainer.style.fontSize = "1.2em";
+        } else if (elementCount === 32) {
+            gameContainer.style.width = "1100px";
+            scoreContainer.style.width = "1070px;";
+            scoreContainer.style.fontSize = "1.5em";
+        }
     }
 }
 
-
-
-let firstClickedImg = null; // Pierwszy kliknięty obrazek
-let secondClickedImg = null; // Drugi kliknięty obrazek
+let firstClickedImg = null;
+let secondClickedImg = null;
 
 function pickImage(e) {
     if (!e.target.classList.contains('img-container')) return;
     const imgContainer = e.target;
 
+    if (imgContainer.classList.contains('clicked')) {
+        return;
+    }
+
     const imgElement = imgContainer.querySelector('.img');
 
     if (!firstClickedImg) {
-        // Pierwszy kliknięty obrazek
         firstClickedImg = imgContainer;
         setTimeout(() => {
             imgElement.style.removeProperty('display');
@@ -84,16 +74,13 @@ function pickImage(e) {
         imgContainer.classList.add('clicked');
 
     } else if (!secondClickedImg) {
-        // Drugi kliknięty obrazek
         secondClickedImg = imgContainer;
         setTimeout(() => {
             imgElement.style.removeProperty('display');
         }, 300);
         imgContainer.classList.add('clicked');
 
-        // sprawdzam, czy oba obrazy mają takie same data-id
         if (firstClickedImg.dataset.id === secondClickedImg.dataset.id) {
-            // Jeśli tak, zostawiamy obrazy odkryte, a zmienne zerujemy, żeby po chwili móc znowu odkrywać
             firstClickedImg = null;
             secondClickedImg = null;
             pairCounter++;
@@ -101,7 +88,6 @@ function pickImage(e) {
             setScoreValue()
 
         } else {
-            // Jeśli różne, po chwili ukrywamy obrazy z powrotem, a zmienne również zerujemy
             attemptsCounter++;
             setScoreValue()
             setTimeout(() => {
@@ -119,10 +105,12 @@ function pickImage(e) {
 document.addEventListener("click", pickImage);
 
 function setScoreValue() {
-    pairs.innerText = quantity;
-    uncoveredPairs.innerText = pairCounter;
-    pairs.innerText = quantity;
-    attempts.innerText = attemptsCounter;
+    const level = document.querySelector(".level")
+    const allAttempts = document.querySelector(".all-attempts")
+    document.querySelector(".pairs").innerText = quantity;
+    document.querySelector(".uncovered-pairs").innerText = pairCounter;
+    document.querySelector(".attempts").innerText = attemptsCounter;
+
     if (quantity === '8') {
         level.innerText = ' łatwy';
         allAttempts.innerText = Number(quantity) * 3;
@@ -133,22 +121,23 @@ function setScoreValue() {
     }
     if (quantity === '16') {
         level.innerText = ' trudny';
-        allAttempts.innerText = Number(quantity) * 4;
+        allAttempts.innerText = Number(quantity) * 3;
     }
 
     if (Number(quantity) === pairCounter) {
-        gameContainer.innerHTML = `<div class="end-text"><h1>Udało Ci się!</h1>
-        <p>Odkryłeś wszystkie pary wykorzystując ${attemptsCounter} próby</p>
-        <button class="end-btn new-game-btn">Zagraj jeszcze raz!</button></div>`
-        scoreContainer.style.display = "none";
-        // endBtn.addEventListener("click", newGame)
+        setTimeout(() => {
+            gameContainer.innerHTML = `<div class="end-text"><h1>Udało Ci się!</h1>
+            <p>Odkryłeś wszystkie pary wykorzystując ${attemptsCounter} próby</p>
+            <button class="end-btn new-game-btn">Zagraj jeszcze raz!</button></div>`
+            scoreContainer.style.display = "none";
+        }, 2000)
+
     }
     else if (Number(quantity) * 3 === attemptsCounter) {
         gameContainer.innerHTML = `<div class="end-text"><h1>Niestety nie udało się!</h1>
         <p>Wykorzystałeś wszystkie możliwe próby</p>
         <button class="end-btn new-game-btn">Spróbuj ponownie!</button></div>`
         scoreContainer.style.display = "none";
-        // endBtn.addEventListener("click", newGame)
     }
 }
 
@@ -168,10 +157,4 @@ function render() {
 }
 
 render()
-
-//Co w następnej kolejności?
-
-
-//5.Karta końcowa 
-
 
